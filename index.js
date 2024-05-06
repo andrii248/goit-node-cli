e608c36db7b490db57383432a58eb8c6543d9441
Andrii Krupa <krupaandrii@gmail.com>
2024-05-06 17:18:30 +0200

    Node CLI submit

---
+++ index.js
@@ -0,0 +1,41 @@
+import { program } from "commander";
+import * as contactsService from "./contacts.js";
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const options = program.opts();
+
+async function invokeAction({ action, id, ...data }) {
+  switch (action) {
+    case "list":
+      const allContacts = await contactsService.listContacts();
+      return console.log(allContacts);
+
+    case "get":
+      const contactById = await contactsService.getContactById(id);
+      return console.log(contactById);
+
+    case "add":
+      const addContact = await contactsService.addContact(data);
+      return console.log(addContact);
+
+    case "remove":
+      const removedContact = await contactsService.removeContact(id);
+      return console.log(removedContact);
+
+    case "update":
+      const updateContact = await contactsService.updateById(id, data);
+      return console.log(updateContact);
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(options);
